d66434ea1492a10d6faa76eeede348b7db734c37
joseph161 <joseph.alimoot@bicol-u.edu.ph>
2015-01-04 21:10:10 +0800

    Theme Park --- Proto-kun
    alalalalalalala

---
+++ Assets/Editor/SomeSampleScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SomeSampleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//Comment to test if editing in browser will affect the local script.
+	}
+}
+// F*c* this s*i*
